e4af9040747fc8252a452c670550944f50bd60a5
r_shibata <ryosuke9913@gmail.com>
2025-08-17 15:48:27 +0900

    init

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shibryo/greeting"
+)
+
+func main() {
+	fmt.Println(greeting.Do())
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shibryo/greeting
+
+go 1.23.6
